433db2f329e9f76ba9ce22a0c3e9915df5dd36c0
Helder Duarte <heldertz2@gmail.com>
2026-02-18 01:44:36 +0000

    Fix LRUCache, unneeded size attribute

--- include/cache.h
+++ include/cache.h
@@ -11,28 +11,25 @@ namespace hd {
 template <typename TKey, typename TValue>
 class LRUCache {
 private:
-    size_t size;
     const size_t capacity;
     std::list<std::pair<TKey, TValue>> list;
     std::unordered_map<TKey, typename std::list<std::pair<TKey, TValue>>::iterator> map;
 
 public:
-    LRUCache(const size_t size) : size(0U), capacity(size), list{}, map{} {}
+    LRUCache(const size_t size) : capacity(size), list{}, map{} {}
 
     void set(TKey key, TValue value) {
         auto it = map.find(key);
         if (it != map.end()) {
             list.erase((*it).second);
             map[key] = list.insert(list.end(), std::make_pair(key, value));
-            size++;
         } else {
-            if (size == capacity) {
+            if (list.size() == capacity) {
                 std::pair<TKey, TValue> head = *list.begin();
                 list.erase(list.begin());
                 map.erase(head.first);
             }
             map[key] = list.insert(list.end(), std::make_pair(key, value));
-            size++;
         }
     }
 
